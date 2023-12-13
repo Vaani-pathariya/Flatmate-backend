054f85ca4eaf859e939a2209a68b670b2d163390
Vaani <vaanipathariya@gmail.com>
2023-12-13 18:50:29 +0530

    adding simple index.js file

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(cors());
 
-// Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/details', (req, res) => {
-    res.send({data: 'Hello World, from express'});
+app.get('/', (req, res) => {
+    res.send('Hello this is your nodejs backend!');
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
