91b68aae8c9b0a3311a7a13bed8d23ca2ee48643
Vaani <vaanipathariya@gmail.com>
2023-12-30 23:12:50 +0530

    changes

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
-const User = require("./models");
+const userModel = require("./models");
 
 const app = express();
 const port = 3000;
@@ -33,77 +31,5 @@ app.get('/', async (req, res) => {
     const user = await userModel.find({});
     res.send(user);
 });
-
-// Signup route
-app.post('/signup', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        // Check if the user with the given email already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: 'User with this email already exists' });
-        }
-
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create a new user
-        const newUser = new User({ email, password: hashedPassword });
-        await newUser.save();
-
-        res.status(201).json({ message: 'User created successfully' });
-    } catch (error) {
-        console.error('Error creating user:', error.message);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-// Login route
-app.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        // Check if the user with the given email exists
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
-
-        // Compare the entered password with the hashed password in the database
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
-
-        // Generate JWT token
-        const token = jwt.sign({ id: user._id, email: user.email }, 'your_jwt_secret_key', { expiresIn: '1h' });
-
-        res.json({ token });
-    } catch (error) {
-        console.error('Error during login:', error.message);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-// Protected route that requires a valid JWT token
-app.get('/protected', verifyToken, (req, res) => {
-    res.json({ message: 'Protected route', user: req.user });
-});
-
-function verifyToken(req, res, next) {
-    const token = req.headers.authorization;
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-
-    jwt.verify(token, 'your_jwt_secret_key', (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: 'Invalid token' });
-        }
-        req.user = decoded;
-        next();
-    });
-}
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
