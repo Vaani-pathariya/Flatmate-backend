95e7af4b021c39deac244de973c85ca07f818ba0
Vaani <vaanipathariya@gmail.com>
2024-01-06 22:52:58 +0530

    changes to schema

--- models.js
+++ models.js
@@ -8,64 +8,103 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
+        default:null,
     },
     capacity:{
         type:Number,
+        default:null,
     },
     drink:{
         type: Boolean,
+        default:null,
     },
     bio:{
         type:String,
+        default:null,
     },
     smoke:{
         type: Boolean,
+        default:null,
     },
     workout:{
         type: Boolean,
+        default:null,
     },
     occupied:{
         type:Number,
+        default:null,
     },
     furnishingStatus: {
         type: String,
         enum: ['semiFurnished', 'fullyFurnished', 'nonFurnished'],
+        default: null
     },
     address: {
-        flat: String,
-        area: String,
-        additional: String,
+        flat: {
+            type: String,
+            default:null,
+        },
+        area:  {
+            type: String,
+            default:null,
+        },
+        additional:  {
+            type: String,
+            default:null,
+        },
     },
     rent: {
-        monthlyAmount: Number,
-        brokerage: Number,
+        monthlyAmount:  {
+            type: Number,
+            default:null,
+        },
+        brokerage:  {
+            type: Number,
+            default:null,
+        },
     },
     dob: {
-        day: Number,
-        month: Number,
-        year: Number
+        day: {
+            type: Number,
+            default:null,
+        },
+        month: {
+            type: Number,
+            default:null,
+        },
+        year: {
+            type: Number,
+            default:null,
+        },
     },
     hasFlat: {
         type: Boolean,
+        default: null
     },
     branch:{
         type: String,
+        default: null
     },
     year:{
         type: Number,
+        default: null
     },
     gender:{
         type: String,
-        enum: ['female', 'male'],
+        enum: ['female', 'male',null],
+        default: null
     },
     googleId: {
         type: String,
+        default: null
     },
     name:{
         type: String,
+        default: null
     },
     picture:{
         type:String,
+        default: null
     },
 });
 
