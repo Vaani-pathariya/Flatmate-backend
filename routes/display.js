addc7527ab9a1b5ea27bfacf739f3f8b062f8cba
Vaani <vaanipathariya@gmail.com>
2024-02-11 15:53:28 +0530

    router.get() error

--- routes/display.js
+++ routes/display.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authenticateToken = require("../middlewares");
 const { getFlats, getFlatmates } = require("../controllers/display");
-router.get("/flats", authenticateToken, getFlats);
-router.get("/flatmates", authenticateToken, getFlatmates);
+// router.get("/flats", authenticateToken, getFlats);
+// router.get("/flatmates", authenticateToken, getFlatmates);
 module.exports = router;
